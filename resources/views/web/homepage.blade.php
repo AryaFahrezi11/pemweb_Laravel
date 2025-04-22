eb67926ed9d530651da2b3e6a4e4b82cd311f649
AryaFahrezi11 <aryamaroely86@gmail.com>
2025-04-22 18:46:26 +0700

    [Database - Menambahkan tabel Products]

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<!-- <x-layout>
     <h3>Ini adalah halaman homepage</h3>
     <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
@@ -11,4 +11,25 @@
 
 <div id="liveAlertPlaceholder"></div>
 <button type="button" class="btn btn-primary" id="liveAlertBtn">Show live alert</button>
-</x-layout>
+</x-layout> -->
+
+<x-layout> 
+ 
+   <div class="row"> 
+       <h3>Categories</h3> 
+       @foreach($categories as $category) 
+           <div class="col-2"> 
+               <div class="card"> 
+                   <img src="{{ $category['image'] }}" class="card-img-top" alt="..."> 
+                   <div class="card-body"> 
+                     <h5 class="card-title">{{ $category['name'] }}</h5> 
+                     <p class="card-text"> 
+                       {{ $category['description'] }} 
+                     </p> 
+                     <a href="/category/{{ $category['slug'] }}" class="btn btn-primary">Detail</a> 
+                   </div> 
+               </div> 
+           </div> 
+       @endforeach 
+   </div> 
+</x-layout> 
--- routes/web.php
+++ routes/web.php
@@ -2,41 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\HomepageController; 
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [HomepageController::class, 'index']); 
+Route::get('products', [HomepageController::class, 'products']); 
+Route::get('product/{slug}', [HomepageController::class, 'product']); 
+Route::get('categories',[HomepageController::class, 'categories']); 
+Route::get('category/{slug}', [HomepageController::class, 'category']); 
+Route::get('cart', [HomepageController::class, 'cart']); 
+Route::get('checkout', [HomepageController::class, 'checkout']); 
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
 
-Route::get('/home', function () {
-    $title = "Homepage";
-    return view('web.homepage',['title'=>$title]);
-});
+// Route::get('/home', function () {
+//     $title = "Homepage";
+//     return view('web.homepage',['title'=>$title]);
+// });
 
 // Menampilkan daftar produk
-Route::get('products', function () {
-    $title = "products";
-    return view('web.products',);
-});
+// Route::get('products', function () {
+//     $title = "products";
+//     return view('web.products',);
+// });
 
-Route::get('products/{slug}', function ($slug) {
-    return view('web.single_products');
-});
+// Route::get('products/{slug}', function ($slug) {
+//     return view('web.single_products');
+// });
 
-Route::get('category', function () {
-    return view('web.category');
-});
+// Route::get('category', function () {
+//     return view('web.category');
+// });
 
-Route::get('category/{slug}', function ($slug) {
-    return view('web.single_category');
-});
+// Route::get('category/{slug}', function ($slug) {
+//     return view('web.single_category');
+// });
 
-Route::get('cart', function () {
-    return view('web.cart');
-});
+// Route::get('cart', function () {
+//     return view('web.cart');
+// });
 
-Route::get('checkout', function () {
-    return view('web.checkout');
-});
+// Route::get('checkout', function () {
+//     return view('web.checkout');
+// });
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
